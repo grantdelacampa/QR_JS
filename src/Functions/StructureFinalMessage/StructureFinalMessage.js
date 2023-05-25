d39eebcf3f2e9825d3673664f5bd82a66bf773ba
Grant De La Campa <grantdelacampa@gmail.com>
2023-05-24 22:53:22 -0700

    started interlevening

--- src/Functions/StructureFinalMessage/StructureFinalMessage.js
+++ src/Functions/StructureFinalMessage/StructureFinalMessage.js
@@ -2,21 +2,32 @@ import { Groups } from "../../Objects/Groups";
 import { GenerateErrorCode } from "../ErrorCorrectionCoding/ErrorCorrectionCoding";
 export function StructureFinalMessage(dataCodeWordGroups, errorCorrectionInfo){
     let response = "";
+    const errorCodewordGroups = generateErrorCodeWords(dataCodeWordGroups, errorCorrectionInfo);
     // This is the best possible case for complexity
-    const errorCodewordGroups = generateCodeWords(dataCodeWordGroups, errorCorrectionInfo);
     if(errorCorrectionInfo.length < 6 && errorCorrectionInfo[2] === 1){
         // No interleaving is necessary place the error correction codewords after the data codewords
-        // TODO: Process the error codewords to binary
         response = dataCodeWordGroups[0].blocks[0].join('') + errorCodewordGroups[0].blocks[0].join('');
     } else {
-        // TODO:
-        // Interleave the data to form the final message
-        // iterate on groups => blocks => codewords/block
+        /*
+        take the first data codeword from the first block
+        followed by the first data codeword from the second block
+        followed by the first data codeword from the third block
+        followed by the first data codeword from the fourth block
+        followed by the second data codeword from the first block
+        */
+       const interlevenCodeWords = [];
+       
     }
     return response;
 }
 
-function generateCodeWords(dataCodeWordGroups, errCorrectionInfo){
+/**
+ * Break down the groups -> blocks and generate the errorCode blocks required
+ * @param {[]} dataCodeWordGroups 
+ * @param {[]} errCorrectionInfo 
+ * @returns 
+ */
+function generateErrorCodeWords(dataCodeWordGroups, errCorrectionInfo){
     // Find the group count
     const errorCodeGroups = dataCodeWordGroups.length > 1 ? [new Groups(), new Groups()] : [new Groups()];
     const errorCodesPerBlock = errCorrectionInfo[1];
