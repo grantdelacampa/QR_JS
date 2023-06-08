f5e80b3152821f48e6140060ce6c154eb72813b9
Grant De La Campa <grantdelacampa@gmail.com>
2023-06-07 23:23:26 -0700

    finished datapattern algo

--- src/Functions/MatrixPopulation/DataPattern.js
+++ src/Functions/MatrixPopulation/DataPattern.js
@@ -2,33 +2,48 @@
 import { splitIntoGroups } from '../../Helpers/HelperFunctions';
 
 export const DataPattern = (bitMatrix, data) => {
-  /*
-    X-XO-XO-XO-XO-XO-XO
-    X-XO-XO-XO-XO-XO-XO
-    X-XO-XO-XO-XO-XO-XO
-    X-XO-XO-XO-XO-XO-XO
-    X-XO-XO-XO-XO-XO-XO
-    X-XO-XO-XO-XO-XO-XO
-    */
-  const bitArray = splitIntoGroups(data, 1);
+  const bitArray = splitIntoGroups(data, 1)
   let dir = true;
   // Traverse each column
-  for(let c = bitMatrix.size; c > -1; c-- ){
+  for(let c = bitMatrix.size-1; c > -1; c-- ){
+    // skip timing pattern (always at 6)
+    if(c === 6){
+        continue;
+    }
 
     // if dir then traverse the column upwards by row
     if(dir){
-        for(let r = bitMatrix.size; r > -1; r--){
-            console.log("^["+ r + ", " + c + "]");
+        for(let r = bitMatrix.size -1; r > -1; r--){
+            if(!bitMatrix.isReserved(r, c)){
+                // pop the first bit off the array
+                const bit = bitArray.shift();
+                // console.log("Writting bit: " + bit + " at ["+ r + ", " + c + "]");
+            }
+            if(!bitMatrix.isReserved(r, (c-1))){
+                const bit = bitArray.shift();
+                // console.log("Writting bit: " + bit + " at ["+ r + ", " + (c-1) + "]");
+            }
         }
         // flip the directiom
         dir = false;
+        // Account for the extra col check
+        c--;
     // else then traverse the column downwards by row
     } else{
         for(let r = 0; r < bitMatrix.size; r++){
-            console.log("v["+ r + ", " + c + "]");
+            if(!bitMatrix.isReserved(r, c)){
+                const bit = bitArray.shift();
+                // console.log("Writting bit: " + bit + " at ["+ r + ", " + c + "]");
+            }
+            if(!bitMatrix.isReserved(r, (c-1))){
+                const bit = bitArray.shift();
+                // console.log("Writting bit: " + bit + " at ["+ r + ", " + (c-1) + "]");
+            }
         }
         // flip the direction
         dir = true;
+        // Account for the extra col check
+        c--;
     }
   }
 };
