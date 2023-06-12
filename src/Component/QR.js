d3394bcb512c75db6ac5253180a2ee94be8fd2f3
Grant De La Campa <grantdelacampa@gmail.com>
2023-06-11 22:43:28 -0700

    QR now draws (small)

--- src/Component/QR.js
+++ src/Component/QR.js
@@ -13,9 +13,27 @@ export const QR = ({
   useEffect(() => {
     const finalMessage = QRCodeGenerator(data, errorCorrection);
     const ctx = canvasRef.current.getContext('2d');
-    ctx.fillStyle = 'gray';
-    ctx.fillRect(0, 0, size, size);    
-    console.log(finalMessage);
+    const imagedata = ctx.createImageData(21, 21);
+    const imgData = imagedata.data;
+    for(let i = 0; i< finalMessage.size; i++){
+      for(let j = 0; j < finalMessage.size; j++){
+        const bit = finalMessage.getBit(i, j);
+        const offset = ((i*finalMessage.size)+j) * 4;
+        if(bit === 1){
+          imgData[offset] = 0;
+          imgData[offset + 1] = 0;
+          imgData[offset + 2] = 0;
+          imgData[offset + 3] = 255;
+        } else {
+          imgData[offset] = 255;
+          imgData[offset + 1] = 255;
+          imgData[offset + 2] = 255;
+          imgData[offset + 3] = 255;
+        }
+      }
+    }
+    ctx.imageSmoothingEnabled = false;
+    ctx.putImageData(imagedata, 0, 0);
   });
   return <canvas id={id} height={size} width={size} ref={canvasRef} />;
 };
